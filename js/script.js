9bd3ebd8c29f0e360b703ea5ff5ae9cffced1a9a
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2022-11-13 09:50:32 -0600

    Add petAdoptionList variable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-alert('Hello World');
-
-let favoriteFood = 'Strawberries';
-document.write(favoriteFood);
+let petAdoptionList = [
+    {
+        name: 'Chloe',
+        age: 4,
+        petColors: [
+            'red' , 'brown'
+        ]
+    },
+    {
+        name: 'Savannah',
+        age: 2,
+        petColors: [
+            'black' , 'white'
+        ]
+    },
+    {
+        name: 'Buster',
+        age: 7,
+        petColors: [
+            'brown' , 'white'
+        ]
+    },
+    {
+        name: 'Piper',
+        age: 10,
+        petColors: [
+            'brown' , 'black'
+        ] 
+    }
+];
